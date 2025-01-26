8d83bb80856fc0383c25c283dc03cf4d0ca3c8e3
Pulkit Dhingra <gl24171@bristol.ac.uk>
2025-01-26 01:51:01 +0000

    Update daily_update.py

--- daily_update.py
+++ daily_update.py
@@ -3,4 +3,4 @@ from datetime import datetime
 now = datetime.now()
 num = random.randint(1, 101)
 with open('/tmp/rand.txt', 'a') as f:
-f.write('{} - Your random number is {}\n'.format(now, num))
+  f.write('{} - Your random number is {}\n'.format(now, num))
